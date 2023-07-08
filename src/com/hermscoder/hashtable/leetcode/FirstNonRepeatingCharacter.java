b13a1dc6b51d0df5915581f248f57cd750f3c3a6
Emerson Ribeiro <emersonrjr03@gmail.com>
2023-07-08 16:41:39 +0100

    Solving exercises "First Non-Repeating Character"

---
+++ src/com/hermscoder/hashtable/leetcode/FirstNonRepeatingCharacter.java
@@ -0,0 +1,27 @@
+package com.hermscoder.hashtable.leetcode;
+
+import java.util.HashMap;
+
+public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        System.out.println(firstNonRepeatingChar("leetcode"));
+        System.out.println(firstNonRepeatingChar("hello"));
+        System.out.println(firstNonRepeatingChar("aabbcc"));
+    }
+
+    public static Character firstNonRepeatingChar(String str) {
+        HashMap<Character, Integer> allChars = new HashMap<>();
+
+        for (char c : str.toCharArray()) {
+            allChars.put(c, allChars.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : str.toCharArray()) {
+            if (allChars.get(c) == 1) {
+                return c;
+            }
+        }
+
+        return null;
+    }
+}
